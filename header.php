db0bb150cb2cb8e7edb7e5d803891041dc620bce
zolaFotsiny <98031811+zolaFotsiny@users.noreply.github.com>
2022-05-17 08:46:10 +0100

    'farany'

--- header.php
+++ header.php
@@ -1,6 +1,10 @@
 <nav class="navbar navbar-expand-lg">
         <div class="container">
-           
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
