6b36e97a220d4a8975c21280b1af36ece43e47c3
zolaFotsiny <98031811+zolaFotsiny@users.noreply.github.com>
2022-05-17 06:57:44 +0100

    'farany'

--- index.php
+++ index.php
@@ -91,24 +91,21 @@ https://templatemo.com/tm-538-digital-trend
                          <div class="owl-carousel owl-theme" id="project-slide">
                               <?php for($i=0;$i<count($Actualite);$i++) {?>
                                    <div class="item project-wrapper" data-aos="fade-up" data-aos-delay="100">
-                                        <img src="images/project/project-image01.jpg" class="img-fluid" alt="project image">
-
-                                        <div class="project-info">
-                                             <small><?php echo $Actualite [$i]['titre'] ?></small>
-
-                                             <h3>
-                                                  <a href="project-detail.html">
-                                                       <span><?php echo $Actualite [$i]['soustitre'] ?></span>
-                                                       <i class="fa fa-angle-right project-icon"></i>
-                                                  </a>
-                                             </h3>
+                                       
+                                        <div class="item project-wrapper" data-aos="fade-up" data-aos-delay="100">
+                                             <div>
+                                                  <center>
+                                                  <h3>
+                                                       <a href="project-detail.html">
+                                                            <span><?php echo $Actualite [$i]['titre'] ?></span>
+                                                       </a>
+                                                  </h3>
+                                                  <small><?php echo $Actualite [$i]['soustitre'] ?></small> 
+                                                  </center>
+                                             </div>
                                         </div>
                                    </div>
                               <?php }?>
-
-                             
-
-                              
                          </div>
                     </div>
 
@@ -121,66 +118,9 @@ https://templatemo.com/tm-538-digital-trend
      
 
 
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
-            <h1 class="text-white" data-aos="fade-up" data-aos-delay="100">We make creative <strong>brands</strong> only.</h1>
-          </div>
-
-          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="200">
-            <h4 class="my-4">Contact Info</h4>
-
-            <p class="mb-1">
-              <i class="fa fa-phone mr-2 footer-icon"></i> 
-              +99 080 070 4224
-            </p>
-
-            <p>
-              <a href="#">
-                <i class="fa fa-envelope mr-2 footer-icon"></i>
-                hello@company.com
-              </a>
-            </p>
-          </div>
-
-          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="300">
-            <h4 class="my-4">Our Studio</h4>
-
-            <p class="mb-1">
-              <i class="fa fa-home mr-2 footer-icon"></i> 
-              Av. Lúcio Costa - Barra da Tijuca, Rio de Janeiro - RJ, Brazil
-            </p>
-          </div>
-
-          <div class="col-lg-4 mx-lg-auto text-center col-md-8 col-12" data-aos="fade-up" data-aos-delay="400">
-            <p class="copyright-text">Copyright &copy; 2020 Your Company
-            <br>
-            <a rel="nofollow noopener" href="https://templatemo.com">Design: TemplateMo</a></p>
-          </div>
-
-          <div class="col-lg-4 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay="500">
-            
-            <ul class="footer-link">
-              <li><a href="#">Stories</a></li>
-              <li><a href="#">Work with us</a></li>
-              <li><a href="#">Privacy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay="600">
-            <ul class="social-icon">
-              <li><a href="#" class="fa fa-instagram"></a></li>
-              <li><a href="#" class="fa fa-twitter"></a></li>
-              <li><a href="#" class="fa fa-dribbble"></a></li>
-              <li><a href="#" class="fa fa-behance"></a></li>
-            </ul>
-          </div>
-
-        </div>
-      </div>
-    </footer>
+     <?php 
+     include("footer.php");
+     ?>
 
 
      <!-- SCRIPTS -->
